c58c0627d0fc7fdf63b7e0ba521a61cb274f9651
ggabi40 <gabibc2000@hotmail.com>
2024-09-16 11:01:03 -0300

    update nav

--- src/app/Components/Nav.tsx
+++ src/app/Components/Nav.tsx
@@ -1,89 +1,95 @@
 import LOGO from "/public/images/LOGO.webp";
 import Image from "next/image";
+import Link from "next/link";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Home() {
   return (
     <>
       <nav>
-        <a className="navbar-brand" href="/">
+        <Link className="navbar-brand" href="/">
           <Image src={LOGO} width={150} height={150} alt="Mundo Juegos" />
-        </a>
+        </Link>
+
+        <Link href="/login" className="user-login">
+          <FontAwesomeIcon icon="user" />
+        </Link>
 
         <div className="categories">
-          {/* Utilizar LINK */}
           {/* juegos/categoria */}
-          <a
+          <Link
             href="../dos-jugadores"
             className="category-link two-players"
           >
             🤼 2 Jugadores
-          </a>
-          <a
+          </Link>
+          <Link
             href="../clasicos"
             className="category-link two-players"
           >
             🎮 Clásicos
-          </a>
-          <a href="../accion" className="category-link action">
+          </Link>
+          <Link href="../accion" className="category-link action">
             ⚔️ Acción
-          </a>
-          <a href="../aventura" className="category-link adventure">
+          </Link>
+          <Link href="../aventura" className="category-link adventure">
             🧭 Aventura
-          </a>
-          <a href="../puzzle" className="category-link puzzle">
+          </Link>
+          <Link href="../puzzle" className="category-link puzzle">
             🧩 Puzzle
-          </a>
-          <a href="../estrategia" className="category-link strategy">
+          </Link>
+          <Link href="../estrategia" className="category-link strategy">
             ♟️ Estrategia
-          </a>
-          <a href="../cartas" className="category-link cards">
+          </Link>
+          <Link href="../cartas" className="category-link cards">
             🃏 Cartas
-          </a>
-          <a href="../deportes" className="category-link sports">
+          </Link>
+          <Link href="../deportes" className="category-link sports">
             ⚽ Deportes
-          </a>
-          <a href="../armas" className="category-link guns">
+          </Link>
+          <Link href="../armas" className="category-link guns">
             🏹 Armas
-          </a>
-          <a href="../cocina" className="category-link kitchen">
+          </Link>
+          <Link href="../cocina" className="category-link kitchen">
             🍔 Cocina
-          </a>
-          <a href="../carreras" className="category-link rides">
+          </Link>
+          <Link href="../carreras" className="category-link rides">
             🏁 Carreras
-          </a>
-          <a href="../autos" className="category-link driving">
+          </Link>
+          <Link href="../autos" className="category-link driving">
             🚗 Autos
-          </a>
-          <a href="../motos" className="category-link bikes">
+          </Link>
+          <Link href="../motos" className="category-link bikes">
             🏍️ Motos
-          </a>
-          <a href="../moda" className="category-link fashion">
+          </Link>
+          <Link href="../moda" className="category-link fashion">
             ✨ Moda
-          </a>
-          <a href="../vestir" className="category-link dress">
+          </Link>
+          <Link href="../vestir" className="category-link dress">
             👕 Vestir
-          </a>
-          <a href="../escape" className="category-link scape">
+          </Link>
+          <Link href="../escape" className="category-link scape">
             🏃 Escape
-          </a>
-          <a href="../terror" className="category-link horror">
+          </Link>
+          <Link href="../terror" className="category-link horror">
             💀 Terror
-          </a>
-          <a href="../medico" className="category-link medic">
+          </Link>
+          <Link href="../medico" className="category-link medic">
             💊 Médico
-          </a>
-          <a
+          </Link>
+          <Link
             href="../educativos"
             className="category-link educational"
           >
             📚 Educativos
-          </a>
-          <a href="../matematicas" className="category-link math">
+          </Link>
+          <Link href="../matematicas" className="category-link math">
             📒 Matemáticas
-          </a>
-          <a href="../musica" className="category-link music">
+          </Link>
+          <Link href="../musica" className="category-link music">
             🎵 Música
-          </a>
+          </Link>
         </div>
 
         <div className="row">
--- src/utils/fontAwesome.ts
+++ src/utils/fontAwesome.ts
@@ -1,4 +1,5 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faYoutube, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faYoutube, faInstagram);
+library.add(faYoutube, faInstagram, faUser);
