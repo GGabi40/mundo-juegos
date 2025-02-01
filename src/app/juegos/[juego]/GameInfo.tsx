7bd2ac6c8b39820e0be5fecc1998259a2f39fb85
ggabi40 <gabibc2000@hotmail.com>
2025-02-01 18:57:39 -0300

    categorias added

--- src/app/juegos/[juego]/GameInfo.tsx
+++ src/app/juegos/[juego]/GameInfo.tsx
@@ -1,15 +1,18 @@
 export default function gameInfo({ params }: { params: { description: string, controls: string } }) {
     return (
         <div className="info">
-        <h2>Descripción del Juego:</h2>
-        <p>{/* Descripción */}
-            {params.description}
-        </p>
-        <h3>Controles:</h3>
-        <p>
-            {/* Controles del juego */}
-            {params.controls}
-        </p>
+            <h2>Descripción del Juego:</h2>
+            <p>{/* Descripción */}
+                {params.description}
+            </p>
+            <h3>Controles:</h3>
+            <p>
+                {/* Controles del juego */}
+                {params.controls}
+            </p>
+            <h3>Categoría:</h3>
+            <div className="categoria">
+            </div>
         </div>
     );
 }
