cbdfe89e5ad7559e0b53c1eb0cf43482e4264c93
ggabi40 <gabibc2000@hotmail.com>
2025-02-16 18:07:05 -0300

    games added

--- src/app/Components/SmallBlocks.tsx
+++ src/app/Components/SmallBlocks.tsx
@@ -4,29 +4,42 @@ import { useState, useEffect } from "react";
 
 import Image from "next/image";
 
+interface Game {
+  id: number;
+  title: string;
+  author: string;
+  image: string;
+  video: string;
+  description: string;
+  iframeCode: string;
+  categories: string[];
+};
 
-export default function SmallBlocks() {
-  const [games, setGames] = useState([]);
-  const [hoveredGame, setHoveredGame] = useState(null);
+export default function SmallBlocks({ category }: { category: string }) {
+  const [games, setGames] = useState<Game[]>([]);
 
   useEffect(() => {
-    fetch('../../public/data/games.json')
+    fetch('/data/games.json')
       .then(response => response.json())
-      .then(data => setGames(data));
+      .then((data: Game[]) => setGames(data))
+      .catch(e => console.error("¡Algo pasó! ", e));
   }, []);
+
+  const getGameByCategory = (category: string) => {
+    return games.filter((game) => game.categories.includes(category));
+  };
+
+  const gamesByCategory = getGameByCategory(category);
 
   return (
     <>
-      <div className="container">
-        <div className="principal-games">
-          <h3>🆕 Novedad</h3>
-            <div className="row w-games">
-            <div className="col">
-              {/* <Image src={} width={100} height={100} alt="Imagen del Juego" /> */}
+      <div className="row w-games">
+        {gamesByCategory.map((game) => (
+            <div key={game.id} className="col">
+              <Image src={game.image} width={100} height={100} alt={game.title} className="game-image" />
+              <h5>{game.title}</h5>
             </div>
-          </div>
-        </div>
-        
+        ))}
       </div>
     </>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,8 +18,17 @@ export default function Home() {
     <>
       <Nav />
       <MainTable />
-      <SmallBlocks />
-      
+
+      <div className="container">
+        <div className="principal-games">
+          <h3>🖱️ Para Cliquear</h3>
+          <SmallBlocks category="Clicker" />
+          <h3>⚔️ Acción</h3>
+          <SmallBlocks category="Action" />
+          <h3>🔷 3D</h3>
+          <SmallBlocks category="3D" />
+        </div>
+      </div>
       <Footer />
     </>
   );
